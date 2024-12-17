5c7b41a5bbe57695bedf679fcced9b5ed1d84f85
mtk30765 <wei.chenglee@mediatek.com>
2024-12-17 17:32:47 +0800

    Refactor dataset loading and model checkpoint handling in main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from pathlib import Path
-
 import torch
 import litgpt
-from datasets import Dataset, load_dataset
-from pydantic import Field, BaseModel
 from lightning import Trainer, LightningModule
-from litgpt.data import JSON
 from litgpt.lora import GPT, mark_only_lora_as_trainable
 from litgpt.utils import chunked_cross_entropy
 
@@ -46,80 +41,9 @@ class FinetuneLLM(LightningModule):
         return [optimizer], [scheduler]
 
 
-class HFDataLoader(BaseModel):
-    # For Loading the dataset from huggingface
-    path: str = Field(
-        ...,
-        title="The path to the dataset",
-        description="The path/url to the dataset from huggingface.",
-        frozen=True,
-        deprecated=False,
-    )
-    name: str = Field(
-        ...,
-        title="The name of the dataset",
-        description="The name of the dataset from huggingface.",
-        frozen=True,
-        deprecated=False,
-    )
-    split: str = Field(
-        "train",
-        title="The split of the dataset",
-        description="The split of the dataset from huggingface.",
-        frozen=True,
-        deprecated=False,
-    )
-
-    # For Litgpt Rules; rename columns to required names
-    question: str = Field(
-        ...,
-        title="The Question of the dataset",
-        description="This is the column name of the question, this field will be renamed to `input`.",
-        frozen=True,
-        deprecated=False,
-    )
-    answer: str = Field(
-        ...,
-        title="The Answer of the dataset",
-        description="This is the column name of the answer, this field will be renamed to `output`.",
-        frozen=True,
-        deprecated=False,
-    )
-    max_cpu: int = Field(
-        default=8,
-        title="The maximum number of CPU cores",
-        description="The maximum number of CPU cores to use for loading the dataset.",
-        frozen=False,
-        deprecated=False,
-    )
-
-    def load(self) -> Dataset:
-        dataset = load_dataset(
-            path=self.path,
-            name=self.name,
-            split=self.split,
-            cache_dir="./data",
-            num_proc=self.max_cpu,
-        )
-        dataset = dataset.rename_columns({self.question: "instruction", self.answer: "output"})
-        need_to_remove = [
-            col for col in dataset.column_names if col not in ["instruction", "output"]
-        ]
-        dataset = dataset.remove_columns(need_to_remove)
-        return dataset
-
-    def load_as_json(self, val_split: float = 0.2) -> JSON:
-        dataset = self.load()
-        filepath = Path(dataset.cache_files[0]["filename"])
-        dataset_path = Path(f"{filepath.parent}/{filepath.stem}.json")
-        dataset.to_json(
-            dataset_path.as_posix(), num_proc=self.max_cpu, force_ascii=False, lines=False
-        )
-        return JSON(json_path=dataset_path, val_split_fraction=val_split)
-
-
 if __name__ == "__main__":
     from litgpt.lora import merge_lora_weights
+    from src.datamodule.data_loader import HFDataLoader
 
     model = "meta-llama/Llama-3.2-1B-Instruct"
     path = "hugfaceguy0001/retarded_bar"
---
+++ src/datamodule/data_loader.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+
+from datasets import Dataset, load_dataset
+from pydantic import Field, BaseModel
+from litgpt.data import JSON
+
+
+class HFDataLoader(BaseModel):
+    # For Loading the dataset from huggingface
+    path: str = Field(
+        ...,
+        title="The path to the dataset",
+        description="The path/url to the dataset from huggingface.",
+        frozen=True,
+        deprecated=False,
+    )
+    name: str = Field(
+        ...,
+        title="The name of the dataset",
+        description="The name of the dataset from huggingface.",
+        frozen=True,
+        deprecated=False,
+    )
+    split: str = Field(
+        "train",
+        title="The split of the dataset",
+        description="The split of the dataset from huggingface.",
+        frozen=True,
+        deprecated=False,
+    )
+
+    # For Litgpt Rules; rename columns to required names
+    question: str = Field(
+        ...,
+        title="The Question of the dataset",
+        description="This is the column name of the question, this field will be renamed to `input`.",
+        frozen=True,
+        deprecated=False,
+    )
+    answer: str = Field(
+        ...,
+        title="The Answer of the dataset",
+        description="This is the column name of the answer, this field will be renamed to `output`.",
+        frozen=True,
+        deprecated=False,
+    )
+    max_cpu: int = Field(
+        default=8,
+        title="The maximum number of CPU cores",
+        description="The maximum number of CPU cores to use for loading the dataset.",
+        frozen=False,
+        deprecated=False,
+    )
+
+    def load(self) -> Dataset:
+        dataset = load_dataset(
+            path=self.path,
+            name=self.name,
+            split=self.split,
+            cache_dir="./data",
+            num_proc=self.max_cpu,
+        )
+        dataset = dataset.rename_columns({self.question: "instruction", self.answer: "output"})
+        need_to_remove = [
+            col for col in dataset.column_names if col not in ["instruction", "output"]
+        ]
+        dataset = dataset.remove_columns(need_to_remove)
+        return dataset
+
+    def load_as_json(self, val_split: float = 0.2) -> JSON:
+        dataset = self.load()
+        filepath = Path(dataset.cache_files[0]["filename"])
+        dataset_path = Path(f"{filepath.parent}/{filepath.stem}.json")
+        dataset.to_json(
+            dataset_path.as_posix(), num_proc=self.max_cpu, force_ascii=False, lines=False
+        )
+        return JSON(json_path=dataset_path, val_split_fraction=val_split)
